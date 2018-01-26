4e61aa2372265ebf4911641af8928e8864b427c3
Jean-Francois Grand <j.grand@oodrive.fr>
2018-01-26 15:57:12 +0100

    Add messaging between popup and background script

--- background.js
+++ background.js
@@ -1,17 +1,33 @@
 const lang = 'French';
+const locale = 'fr';
+
+const baseUrl = 'https://translate.google.fr/#';
+const sourceLang = 'auto';
+let destLang;
+
+const getUri = (toTranslate, languageDestination = 'fr') => encodeURI(`${baseUrl}${sourceLang}/${languageDestination}/${toTranslate}`);
+
+const getTranslation = (info, tab) => {
+    console.log('clicked');
+    if (info.menuItemId === 'translator') {
+        const text = info.selectionText;
+        chrome.tabs.create({
+            "url": getUri(text, destLang),
+        });
+    }
+}
 
 chrome.contextMenus.create({
     id: 'translator',
     title: 'Translate "%s" to ' + lang,
     contexts: ['selection'],
+    onclick: getTranslation,
 });
 
-console.log(chrome.contextMenus.onClicked);
-
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-    console.log('clicked');
-    if (info.menuItemId === 'translator') {
-        const text = info.selection;
-        console.log(text);
+const popupListener = (request, sender, sendResponse) => {
+    if (request.currentLang) {
+        destLang = request.currentLang;
     }
-});
+}
+
+chrome.runtime.onMessage.addListener(popupListener);
--- popup/choose-lang.js
+++ popup/choose-lang.js
@@ -2,11 +2,11 @@
 		Listen popup clicks.
 		On click, sets the translation language.
     */
-
     const popup = document.querySelector('#popup-content');
     popup.addEventListener('click', (e) => {
         if (!e.target.matches('.button')) {
             return;
         }
-        console.log(e.target.dataset.locale);
+        console.log(e.target.dataset.locale)
+        chrome.runtime.sendMessage({ currentLang: e.target.dataset.locale });
     });
