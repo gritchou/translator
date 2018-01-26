8b407425a36846ef3d56421e81bed417abd5c2cb
Jean-Francois Grand <j.grand@oodrive.fr>
2018-01-26 17:43:32 +0100

    Improve popup styles

--- popup/choose-lang.js
+++ popup/choose-lang.js
@@ -7,6 +7,9 @@
         if (!e.target.matches('.button')) {
             return;
         }
+        popup.querySelectorAll('.button').forEach((element) => element.classList.remove('active'));
+        e.target.classList.add('active');
+        console.log(e);
         console.log(e.target.dataset.locale)
         chrome.runtime.sendMessage({ currentLang: e.target.dataset.locale });
     });
