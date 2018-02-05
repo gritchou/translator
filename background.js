ce5c3aa9534bebc21e33ff6b159073184a41871f
Jean-Francois Grand <j.grand@oodrive.fr>
2018-02-05 10:23:56 +0100

    Small updates

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ const langs = {
     'fr': 'French',
     'en': 'English',
     'de': 'German',
+    'uk': 'Ukrainian',
     'nl': 'Dutch',
     'es': 'Spanish',
     'ja': 'Japanese',
@@ -11,32 +12,31 @@ const langs = {
 
 const baseUrl = 'https://translate.google.fr/#';
 const sourceLang = 'auto';
-let destLang = 'fr';
+let targetLang = 'fr';
 
 const getUri = (toTranslate, languageDestination = 'fr') => encodeURI(`${baseUrl}${sourceLang}/${languageDestination}/${toTranslate}`);
 
 const getTranslation = (info, tab) => {
-    console.log('clicked');
     if (info.menuItemId === 'translator') {
         const text = info.selectionText;
         chrome.tabs.create({
-            "url": getUri(text, destLang),
+            "url": getUri(text, targetLang),
         });
     }
 }
 
 chrome.contextMenus.create({
     id: 'translator',
-    title: 'Translate "%s" to ' + langs[destLang],
+    title: 'Translate "%s" to ' + langs[targetLang],
     contexts: ['selection'],
     onclick: getTranslation,
 });
 
 const popupListener = (request, sender, sendResponse) => {
     if (request.currentLang) {
-        destLang = request.currentLang;
+        targetLang = request.currentLang;
         chrome.contextMenus.update('translator', {
-            title: 'Translate "%s" to ' + langs[destLang],
+            title: 'Translate "%s" to ' + langs[targetLang],
         });
     }
 }
