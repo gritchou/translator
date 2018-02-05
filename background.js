6612c3df77a02e84cac07000918b4f43e420680b
Jean-Francois Grand <j.grand@oodrive.fr>
2018-02-05 12:23:37 +0100

    Init

--- background.js
+++ background.js
@@ -12,33 +12,31 @@ const langs = {
 
 const baseUrl = 'https://translate.google.fr/#';
 const sourceLang = 'auto';
-let targetLang = 'fr';
+let targetLang;
 
-const getUri = (toTranslate, languageDestination = 'fr') => encodeURI(`${baseUrl}${sourceLang}/${languageDestination}/${toTranslate}`);
+const getUri = (phraseToTranslate, targetLanguage = 'fr') => encodeURI(`${baseUrl}${sourceLang}/${targetLanguage}/${phraseToTranslate}`);
 
-const getTranslation = (info, tab) => {
-    if (info.menuItemId === 'translator') {
-        const text = info.selectionText;
-        chrome.tabs.create({
-            "url": getUri(text, targetLang),
-        });
-    }
-}
+// const getTranslation = (info, tab) => {
+//     if (info.menuItemId === 'translator') {
+//         const text = info.selectionText;
+//         TODO
+//     }
+// }
 
 chrome.contextMenus.create({
     id: 'translator',
     title: 'Translate "%s" to ' + langs[targetLang],
     contexts: ['selection'],
-    onclick: getTranslation,
+    onclick: TODO,
 });
 
-const popupListener = (request, sender, sendResponse) => {
-    if (request.currentLang) {
-        targetLang = request.currentLang;
-        chrome.contextMenus.update('translator', {
-            title: 'Translate "%s" to ' + langs[targetLang],
-        });
-    }
-}
+// const popupListener = (request, sender, sendResponse) => {
+//     if (request.currentLang) {
+//         targetLang = request.currentLang;
+//         chrome.contextMenus.update('translator', {
+//             title: 'Translate "%s" to ' + langs[targetLang],
+//         });
+//     }
+// }
 
-chrome.runtime.onMessage.addListener(popupListener);
+// TODO
